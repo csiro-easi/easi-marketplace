78e4164568b511b01fae0180e087ed249c883c29
Carsten Friedrich <Carsten.Friedrich@csiro.au>
2018-05-21 16:19:02 +1000

    "About" Box added. Required switch to Angular 6.0.1

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private bottomSheet: MatBottomSheet) {}
+
+  openBottomSheet(): void {
+    this.bottomSheet.open(AboutBottomSheet);
+  }
+}
+
+@Component({
+  selector: 'app-about',
+  templateUrl: 'about.html',
+})
+export class AboutBottomSheet {
+  constructor(private bottomSheetRef: MatBottomSheetRef<AboutBottomSheet>) {}
+
+  openLink(event: MouseEvent): void {
+    this.bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+}
+  
+  
